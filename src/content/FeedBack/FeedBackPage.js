41945aeba3fd569c8f3320814ca293503de15528
zunyinmon <ymonzun@gmail.com>
2023-09-08 15:43:27 +0800

    updated Feedback Page

--- src/content/FeedBack/FeedBackPage.js
+++ src/content/FeedBack/FeedBackPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import BXDropdown from 'carbon-web-components/es/components-react/dropdown/dropdown.js';
+import BXDropdownItem from 'carbon-web-components/es/components-react/dropdown/dropdown-item.js';
+const FeedBackPage = () => (
+  <>
+  <h1>FeedBack Page! 👋</h1>
+  <div id="app">
+  <BXDropdown triggerContent="Select an item">
+  <BXDropdownItem value="all">Option 1</BXDropdownItem>
+  <BXDropdownItem value="cloudFoundry">Option 2</BXDropdownItem>
+  <BXDropdownItem value="staging">Option 3</BXDropdownItem>
+  <BXDropdownItem value="dea">Option 4</BXDropdownItem>
+  <BXDropdownItem value="router">Option 5</BXDropdownItem>
+</BXDropdown>
 
-const FeedBackPage = () => {
-  return <div>FeedBack PAGE</div>;
-};
+
+</div>
+  </>
+)
+  
 
 export default FeedBackPage;
